9b34dea793e96416d27c313311846e17b39b40e5
gufeijun <gugoings@gmail.com>
2022-04-22 13:16:33 +0800

    fix token.go

--- complier/lexer/token.go
+++ complier/lexer/token.go
@@ -1,13 +1,5 @@
 package lexer
 
-type Token struct {
-	Kind    int         // token kind
-	Line    int         // line number
-	Kth     int         // index of first charactor of Content in current line
-	Content string      // original string
-	Value   interface{} // token value, string literal, number literal or identifier
-}
-
 // token kind
 const (
 	TOKEN_EOF        = iota // end of file
@@ -15,10 +7,9 @@ const (
 	TOKEN_NUMBER            // number
 	TOKEN_STRING            // string
 
-	// 分隔符
+	// seperator
+	TOKEN_SEP_DOT    // .
 	TOKEN_SEP_VARARG // ...
-	TOKEN_SEP_DQUOTE // "
-	TOKEN_SEP_SQUOTE // '
 	TOKEN_SEP_COLON  // :
 	TOKEN_SEP_SEMI   // ;
 	TOKEN_SEP_QMARK  // ?
@@ -29,11 +20,12 @@ const (
 	TOKEN_SEP_LCURLY // {
 	TOKEN_SEP_RCURLY // }
 
-	// 操作符
+	// operator
 	TOKEN_OP_ADD    // +
 	TOKEN_OP_SUB    // -
 	TOKEN_OP_MUL    // *
 	TOKEN_OP_DIV    // /
+	TOKEN_OP_IDIV   // //
 	TOKEN_OP_MOD    // %
 	TOKEN_OP_LAND   // &&
 	TOKEN_OP_LOR    // ||
@@ -41,6 +33,7 @@ const (
 	TOKEN_OP_OR     // |
 	TOKEN_OP_XOR    // ^
 	TOKEN_OP_NOT    // ~
+	TOKEN_OP_LNOT   // !
 	TOKEN_OP_EQ     // ==
 	TOKEN_OP_NE     // !=
 	TOKEN_OP_LT     // <
@@ -62,7 +55,7 @@ const (
 	TOKEN_OP_SHL    // <<
 	TOKEN_OP_SHR    // >>
 
-	// 关键字
+	// keywords
 	TOKEN_KW_BREAK       // break
 	TOKEN_KW_CONTINUE    // continue
 	TOKEN_KW_FOR         // for
@@ -86,7 +79,19 @@ const (
 	TOKEN_KW_DELETE      // delete
 	TOKEN_KW_TYPE        // type
 	TOKEN_KW_LOOP        // loop
+	TOKEN_KW_IMPORT      // import
 )
+
+var _eofToken = Token{Kind: TOKEN_EOF}
+var eofToken = &_eofToken
+
+type Token struct {
+	Kind    int         // token kind
+	Line    int         // line number
+	Kth     int         // index of first charactor of Content in current line
+	Content string      // original string
+	Value   interface{} // token value, string literal, number literal or identifier
+}
 
 var keywords = map[string]int{
 	"break":       TOKEN_KW_BREAK,
@@ -112,4 +117,5 @@ var keywords = map[string]int{
 	"delete":      TOKEN_KW_DELETE,
 	"type":        TOKEN_KW_TYPE,
 	"loop":        TOKEN_KW_LOOP,
+	"import":      TOKEN_KW_IMPORT,
 }
