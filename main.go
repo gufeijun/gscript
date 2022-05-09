4205afb25ff5329f9037142f0486096e81bb09f8
gufeijun <gugoings@gmail.com>
2022-05-09 21:22:15 +0800

    .

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"gscript/complier/codegen"
+	"gscript/complier/lexer"
+	"gscript/complier/parser"
+	"gscript/vm"
+)
+
+// 1 2 3 5 8 13 21 34
+const str = `
+func fib(a){
+	if (a==0) return 0;
+	if (a==1) return 1;
+	return fib(a-1)+fib(a-2)
+}
+
+let a=fib(35)
+`
+
+func main() {
+	parser := parser.NewParser(lexer.NewLexer("", []byte(str)))
+	text, consts, ft := codegen.Gen(parser)
+
+	v := vm.NewVM(text, consts, ft)
+	v.Debug()
+}
