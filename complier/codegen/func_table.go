31e5a025d8e3495e55efe568296973e93fc7813a
gufeijun <gugoings@gmail.com>
2022-05-12 00:07:16 +0800

    add anounymousFunc struct

--- complier/codegen/func_table.go
+++ complier/codegen/func_table.go
@@ -2,30 +2,29 @@ package codegen
 
 import (
 	"gscript/complier/ast"
-	"gscript/complier/parser"
 	"gscript/proto"
 )
 
 type FuncTable struct {
-	funcTable []proto.Func
+	funcTable []proto.FuncProto // index -> proto
 	funcMap   map[string]uint32 // funcname -> index
+	funcTexts [][]byte          // index -> bytes code
+
+	anonymousFuncs     []proto.AnonymousFuncProto
+	anonymousFuncTexts [][]byte
 }
 
 func newFuncTable(funcs []*ast.FuncDefStmt) *FuncTable {
 	ft := &FuncTable{
 		funcMap:   make(map[string]uint32),
-		funcTable: make([]proto.Func, len(funcs)),
+		funcTable: make([]proto.FuncProto, len(funcs)),
 	}
 	for i, f := range funcs {
 		ft.funcMap[f.Name] = uint32(i)
-		ft.funcTable[i].Parameters = f.Parameters
-		ft.funcTable[i].VaArgs = f.VaArgs != ""
-		i++
+		info := new(proto.BasicInfo)
+		info.Parameters = f.Parameters
+		info.VaArgs = f.VaArgs != ""
+		ft.funcTable[i].Info = info
 	}
 	return ft
-}
-
-func (ft *FuncTable) addFuncLiteral(literal *ast.FuncLiteral, p *parser.Parser) {
-	p.FuncDefs = append(p.FuncDefs, &ast.FuncDefStmt{Name: "", FuncLiteral: *literal})
-	ft.funcTable = append(ft.funcTable, proto.Func{Parameters: literal.Parameters, VaArgs: literal.VaArgs != ""})
 }
