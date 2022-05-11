4f41347be35699e24687da8768ff13072794c610
gufeijun <gugoings@gmail.com>
2022-05-12 00:18:49 +0800

    test closure

--- main.go
+++ main.go
@@ -7,20 +7,42 @@ import (
 	"gscript/vm"
 )
 
-const str = `
-func fib(a){
-	if (a==0) return 0;
-	if (a==1) return 1;
-	return fib(a-1)+fib(a-2)
+const s = `
+func newCounter(){
+	let count=0
+	return func(){
+		count++
+		return func(){
+			count++
+			return count
+		}
+	}
 }
 
-let a=fib(35)
+let cb = newCounter()
+let a = cb()	# count=1
+let b= cb()		# count=2
+
+let c = a()		# c=3 count=3
+let d = b()		# d=4 count=4
+`
+
+const str = `
+let count = 0;
+
+let result = inc()	// result = count = 1
+
+# closure
+func inc(){
+	count++
+	return count
+}
 `
 
 func main() {
-	parser := parser.NewParser(lexer.NewLexer("", []byte(str)))
-	text, consts, ft := codegen.Gen(parser)
+	parser := parser.NewParser(lexer.NewLexer("", []byte(s)))
+	_proto := codegen.Gen(parser)
 
-	v := vm.NewVM(text, consts, ft)
+	v := vm.NewVM(&_proto)
 	v.Debug()
 }
