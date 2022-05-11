2ea2526fc631c153bf4ba0eaf3e2df12835a6dbe
gufeijun <gugoings@gmail.com>
2022-05-12 00:12:29 +0800

    use struct GsValue to store variable instead of pure interface{}

--- vm/symbol_tabel.go
+++ vm/symbol_tabel.go
@@ -1,7 +1,11 @@
 package vm
 
 type symbolTable struct {
-	values []interface{}
+	values []*GsValue
+}
+
+type GsValue struct {
+	value interface{}
 }
 
 func newSymbolTable() *symbolTable {
@@ -12,16 +16,16 @@ func (st *symbolTable) getValue(idx uint32) interface{} {
 	if idx >= uint32(len(st.values)) {
 		panic("index out of symbol table")
 	}
-	return st.values[idx]
+	return st.values[idx].value
 }
 
 func (st *symbolTable) setValue(idx uint32, val interface{}) {
 	if idx >= uint32(len(st.values)) {
 		panic("index out of symbol table")
 	}
-	st.values[idx] = val
+	st.values[idx].value = val
 }
 
 func (st *symbolTable) pushSymbol(val interface{}) {
-	st.values = append(st.values, val)
+	st.values = append(st.values, &GsValue{val})
 }
