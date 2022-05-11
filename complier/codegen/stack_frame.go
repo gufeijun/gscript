ca7ce43dd32890e68e541386200d9e8f80234167
gufeijun <gugoings@gmail.com>
2022-05-12 00:09:23 +0800

    add struct StackFrame for supporting closure

---
+++ complier/codegen/stack_frame.go
@@ -0,0 +1,22 @@
+package codegen
+
+type StackFrame struct {
+	prev        *StackFrame
+	nt          *NameTable
+	vt          *UpValueTable
+	bs          *blockStack
+	validLabels map[string]label
+	returnAtEnd bool
+	text        []byte
+
+	nowParsingAnonymous int
+}
+
+func newStackFrame() *StackFrame {
+	return &StackFrame{
+		nt:          NewNameTable(),
+		bs:          newBlockStack(),
+		validLabels: map[string]label{},
+		vt:          newUpValueTable(),
+	}
+}
