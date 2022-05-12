64e1af6686a824610f7c07dd96de6af0d9b66c7f
gufeijun <gugoings@gmail.com>
2022-05-12 19:27:45 +0800

    test class statement and new object

--- main.go
+++ main.go
@@ -7,40 +7,24 @@ import (
 	"gscript/vm"
 )
 
-const s = `
-func newCounter(){
-	let count=0
-	return func(){
-		count++
-		return func(){
-			count++
-			return count
-		}
+const code = `
+class people{
+	__self(age,name){
+		this.age = age
+		this.name = name
 	}
+	birthday(){
+		this.age++
+	}
+	location = "wuhan"
 }
 
-let cb = newCounter()
-let a = cb()	# count=1
-let b= cb()		# count=2
-
-let c = a()		# c=3 count=3
-let d = b()		# d=4 count=4
-`
-
-const str = `
-let count = 0;
-
-let result = inc()	// result = count = 1
-
-# closure
-func inc(){
-	count++
-	return count
-}
+let b = new people(10,"Mike")
+b.birthday()  # b.age=11, b.name="Mike", b.location="wuhan"
 `
 
 func main() {
-	parser := parser.NewParser(lexer.NewLexer("", []byte(s)))
+	parser := parser.NewParser(lexer.NewLexer("", []byte(code)))
 	_proto := codegen.Gen(parser)
 
 	v := vm.NewVM(&_proto)
