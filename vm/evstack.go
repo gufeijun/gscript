b990e6515b7b951e4db2fa5ca36bba1f3f013aa8
gufeijun <gugoings@gmail.com>
2022-05-27 14:54:46 +0800

    add some api

--- vm/evstack.go
+++ vm/evstack.go
@@ -16,10 +16,31 @@ func (s *evalStack) Top() interface{} {
 	return s.Buf[len(s.Buf)-1]
 }
 
+func (s *evalStack) top(n int) interface{} {
+	return s.Buf[len(s.Buf)-n]
+}
+
+func (s *evalStack) popN(n int) {
+	last := len(s.Buf)
+	for i := 0; i < n; i++ {
+		last--
+		s.Buf[last] = nil
+	}
+	s.Buf = s.Buf[:last]
+}
+
 func (s *evalStack) Pop() {
 	last := len(s.Buf) - 1
 	s.Buf[last] = nil
 	s.Buf = s.Buf[:last]
+}
+
+func (s *evalStack) pop() (val interface{}) {
+	last := len(s.Buf) - 1
+	val = s.Buf[last]
+	s.Buf[last] = nil
+	s.Buf = s.Buf[:last]
+	return
 }
 
 func (s *evalStack) Push(v interface{}) {
