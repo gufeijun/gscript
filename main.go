d455609aa76754a954e7050298e014611516db77
gufeijun <gugoings@gmail.com>
2022-05-27 14:56:27 +0800

    add .gitignore

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
 package main
 
 import (
-	"gscript/complier/codegen"
-	"gscript/complier/lexer"
-	"gscript/complier/parser"
+	"gscript/complier"
 	"gscript/vm"
 )
 
-const code = `
-class people{
-	__self(age,name){
-		this.age = age
-		this.name = name
-	}
-	birthday(){
-		this.age++
-	}
-	location = "wuhan"
-}
-
-let b = new people(10,"Mike")
-b.birthday()  # b.age=11, b.name="Mike", b.location="wuhan"
-`
-
 func main() {
-	parser := parser.NewParser(lexer.NewLexer("", []byte(code)))
-	_proto := codegen.Gen(parser)
-
-	v := vm.NewVM(&_proto)
+	protos, err := complier.ComplieWithSrcFile("./test.gs")
+	if err != nil {
+		panic(err)
+	}
+	v := vm.NewVM(protos)
 	v.Debug()
 }
