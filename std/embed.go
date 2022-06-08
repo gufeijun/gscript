b729998ec4ded84c8771d3fb5ada01e1061653de
gufeijun <gufeijun@bytedance.com>
2022-06-08 20:27:36 +0800

    chore: embed all gsproto files in std

---
+++ std/embed.go
@@ -0,0 +1,40 @@
+package std
+
+import (
+	"bufio"
+	"embed"
+	"fmt"
+	"gscript/proto"
+)
+
+//go:embed *.gsproto
+var ProtoFiles embed.FS
+
+const protoSuffix string = ".gsproto"
+
+var StdLibs = map[string]uint32{
+	"Buffer": 0,
+	"fs":     1,
+	"os":     2,
+}
+
+func ReadProto(lib string) (proto.Proto, error) {
+	file, err := ProtoFiles.Open(lib + protoSuffix)
+	if err != nil {
+		return proto.Proto{}, fmt.Errorf("can not find std libarary bytes code: %v", err)
+	}
+	defer file.Close()
+	return proto.ReadProto(bufio.NewReader(file))
+}
+
+func ReadProtos() ([]proto.Proto, error) {
+	protos := make([]proto.Proto, len(StdLibs))
+	for lib, num := range StdLibs {
+		p, err := ReadProto(lib)
+		if err != nil {
+			return nil, err
+		}
+		protos[num] = p
+	}
+	return protos, nil
+}
