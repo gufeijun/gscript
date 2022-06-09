461923f494947cf3f5724d8128082e983fc16f09
gufeijun <gugoings@gmail.com>
2022-06-09 17:13:04 +0800

    refactor: use cobra to build cli application

--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"gscript/complier"
-	"gscript/std"
-	"gscript/vm"
+	"gscript/cmd"
 )
 
 func main() {
-	protos, err := complier.ComplieWithSrcFile("./test.gs")
-	if err != nil {
-		panic(err)
-	}
-
-	stdlibs, err := std.ReadProtos()
-	if err != nil {
-		panic(err)
-	}
-
-	v := vm.NewVM(protos, stdlibs)
-	v.Run()
-	// v.Debug()
+	cmd.Execute()
 }
